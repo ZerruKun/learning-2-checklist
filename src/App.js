077944f0667d0fe2dba9c9dfb870b88958b69a92
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-06-21 16:13:51 +0300

    Подготовка App, наполнение базовой логикой отрисовки.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,36 @@ import ToDo from './ToDo';
 import ToDoForm from './ToDoForm';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTask = () => {
+
+  }
+
+  const removeTask = () => {
+
+  }
+
+  const handleToggle = () => {
+
+  }
+
   return (
     <div className="App">
-
+      <header>
+        <h1>Список задач: {todos.length}</h1>
+      </header>
+      <ToDoForm addTask={addTask}/>
+      {todos.map((todo) => {
+        return (
+          <ToDo
+            todo={todo} 
+            key={todo.id}
+            toggleTask={handleToggle}
+            removeTask={removeTask} 
+            />
+        )
+      })}
     </div>
   );
 }
