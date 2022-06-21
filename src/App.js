b8e0f653bb5ef9a5b7b4722a466c4b4f39d29464
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-06-21 15:53:21 +0300

    Создание пустых компонентов (подготовка), импорт в App.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import {useState} from 'react';
+import ToDo from './ToDo';
+import ToDoForm from './ToDoForm';
+
 function App() {
   return (
     <div className="App">
---
+++ src/ToDoForm.jsx
@@ -0,0 +1,5 @@
+const ToDoForm = () => {
+    return null;
+}
+
+export default ToDoForm;
